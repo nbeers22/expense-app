20d56e034a1363a4e66bbd6f19603ffc671b5e75
Nathan Beers <nbeers22@gmail.com>
2015-07-08 17:01:17 -0400

    Fixed debits and credits so the total only applies to a specific user and not entire database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
-
 	def index
-		user = current_user
-		@activities = user.activities
+		if current_user
+			user = current_user
+			@activities = user.activities 
+			@debits = user.activities.where(expense_type: "Debit").pluck(:amount).inject(:+)
+			@credits = user.activities.where(expense_type: "Credit").pluck(:amount).inject(:+)
+		end
 	end
 end
