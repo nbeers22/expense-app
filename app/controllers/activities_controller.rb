0f898550e0eeb507b536d35c7445638d2520debe
Nathan Beers <nbeers22@gmail.com>
2015-07-10 11:18:58 -0400

    Added july expense report

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
-	before_action :find_user, only: [:create,:edit,:index]
+	before_action :find_user, only: [:create,:edit,:index,:show]
 	before_action :find_activity, only: [:update,:destroy]
 
 	def new
@@ -29,6 +29,18 @@ class ActivitiesController < ApplicationController
 		root
 	end
 
+	def show
+		@july_2015 = @user.activities.where(created_at: Date.parse("2015-7-1").beginning_of_day..Date.parse("2015-7-31").end_of_day)
+		@july_2015_debit = july_debit
+		@july_2015_credit = july_credit
+		
+		@aug_2015 = @user.activities.where(created_at: Date.parse("2015-8-1").beginning_of_day..Date.parse("2015-8-31").end_of_day)
+		@sept_2015 = @user.activities.where(created_at: Date.parse("2015-9-1").beginning_of_day..Date.parse("2015-9-30").end_of_day)
+		@oct_2015 = @user.activities.where(created_at: Date.parse("2015-10-1").beginning_of_day..Date.parse("2015-10-31").end_of_day)
+		@nov_2015 = @user.activities.where(created_at: Date.parse("2015-11-1").beginning_of_day..Date.parse("2015-11-30").end_of_day)
+		@dec_2015 = @user.activities.where(created_at: Date.parse("2015-12-1").beginning_of_day..Date.parse("2015-12-31").end_of_day)
+	end
+
 	private
 
 	def find_user
@@ -45,5 +57,13 @@ class ActivitiesController < ApplicationController
 
 	def activities_params
 		params.require(:activity).permit(:title, :expense_type, :amount, :category)
+	end
+
+	def july_debit
+		@user.activities.where(expense_type: "Debit", created_at: Date.parse("2015-7-1").beginning_of_day..Date.parse("2015-7-31").end_of_day).pluck(:amount).inject(:+)
+	end
+
+	def july_credit
+		@user.activities.where(expense_type: "Credit", created_at: Date.parse("2015-7-1").beginning_of_day..Date.parse("2015-7-31").end_of_day).pluck(:amount).inject(:+)
 	end
 end
